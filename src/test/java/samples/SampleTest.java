1c9773a0fb220ee3575d1b37337035311589b193
yuliiashevchenko <jullshevchenko@gmail.com>
2017-10-18 14:25:38 +0300

    adding gitignore file

---
+++ src/test/java/samples/SampleTest.java
@@ -0,0 +1,29 @@
+package samples;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by YS on 10/18/17
+ */
+
+
+    @Test
+public class SampleTest {
+    public void first(){
+        System.out.println("1");
+        Assert.assertEquals("2", "1");
+
+      }
+    @Test
+    public void second() {
+        System.out.println("2");
+        }
+    @Test
+    public void third() {
+        System.out.println("3");
+
+        }
+    }
+
+
